164c103bb368e81f917d4975ca4e6f3e531891e5
Derwin McGeary <derwinmcgeary+web@gmail.com>
2015-11-22 00:18:14 +0000

    Improve documentation, put everything in server.R for reasons of grading

--- ui.R
+++ ui.R
@@ -1,7 +1,11 @@
 library(shiny)
 shinyUI(pageWithSidebar(
   headerPanel("Stuff to do in Town"),
-  sidebarPanel(sliderInput('time','Choose a time', value=as.numeric(strftime(Sys.time(),"%H")), min=6,max=23,step=1),
+  sidebarPanel(p("This app uses data from open.glasgow.gov.uk to display what public buildings are open in Glasgow and how long for."),
+               h4("Documentation"),
+               p("Choose a time and day by clicking below! The default is simply the current time. You can check which things are open latest, or what kind of facilities open at 7 in the morning, for example."),
+               sliderInput('time','Choose a time', 
+                           value=as.numeric(strftime(Sys.time(),"%H")), min=6,max=23,step=1),
                selectInput('day',"Choose a day",
                            list("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"), selected=weekdays(Sys.time()))
                ),
